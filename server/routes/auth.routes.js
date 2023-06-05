80bf354a2774a8f9e699e2b98d45305f33d0fed0
Camila Buldin <cami@Camilas-MacBook-Air.local>
2023-06-05 10:45:32 +0200

    demas

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,6 +3,7 @@ const User = require("../models/User.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const isAuthenticated = require("../middleware/jwt.middleware");
+const fileUploader = require("../config/cloudinary.config");
 
 const saltRounds = 10;
 
@@ -65,6 +66,22 @@ router.post("/login", (req, res, next) => {
 //GET -> http://localhost:5005/auth/verify
 router.get("/verify", isAuthenticated, (req, res, next) => {
   res.json(req.payload);
+});
+
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("picture"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+
+  res.json({ fileUrl: req.file.path });
 });
 
 module.exports = router;
