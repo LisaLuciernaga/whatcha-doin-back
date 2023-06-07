596654e511a8d899edc3e3f163b42a5f25df21e6
Lisa Schwetlick <schwetlick.lisa@gmail.com>
2023-06-07 09:37:59 +0200

    fixing populate in user route, event to be created in events route

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -9,9 +9,10 @@ router.get("/:username", (req, res, next) => {
   let { username } = req.params;
   User.findOne({ username: username })
     //populate friends, events & store in payload changeLater
-    .populate("friendsConfirmed", "inviteLists" )
-    .populate("friendsPending", "notifications")
-    //friendsPending notifications"
+    .populate("friendsConfirmed")
+    .populate("inviteLists")
+    .populate("friendsPending")
+    .populate("notifications")
     .then((resp) => {
       res.json(resp);
     })
