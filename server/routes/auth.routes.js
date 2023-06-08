36bb92edef664e6c941610f339bc705633de9aae
Raquel Barrio <r.barrio.f@gmail.com>
2023-06-08 23:54:53 +0200

    give up, doesn't work

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,13 +3,12 @@ const User = require("../models/User.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const isAuthenticated = require("../middleware/jwt.middleware");
-const fileUploader = require("../config/cloudinary.config")
 
 const saltRounds = 10;
 
 //POST -> http://localhost:5005/auth/signup
 router.post("/signup", (req, res, next) => {
-  const { username, email, password, passwordRepeat, picture } = req.body;
+  const { username, email, password, passwordRepeat } = req.body;
   // console.log("backend username:", user)
 
   if (username == "" || email == "" || password == "" || passwordRepeat == "") {
@@ -26,7 +25,7 @@ router.post("/signup", (req, res, next) => {
       let salt = bcrypt.genSaltSync(saltRounds);
       let passwordEnc = bcrypt.hashSync(password, salt);
 
-      return User.create({ username, email, password: passwordEnc, picture });
+      return User.create({ username, email, password: passwordEnc });
     })
     .then((user) => {
       res.json(user);
@@ -66,42 +65,11 @@ router.post("/login", (req, res, next) => {
     .catch((err) => next(err));
 });
 
-// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
-//poner :userid en la ruta!!! (??????)
-router.post("/upload", fileUploader.single("picture"), (req, res, next) => {
-  // console.log("file is: ", req.file)
- 
-  if (!req.file) {
-    next(new Error("No file uploaded!"));
-    return;
-  }
-
-  // Get the URL of the uploaded file and send it as a response.
-  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
-
-  res.json({ fileUrl: req.file.path });
-});
-
 
 //GET -> http://localhost:5005/auth/verify
 router.get("/verify", isAuthenticated, (req, res, next) => {
   res.json(req.payload);
 });
 
-
-// // POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
-// router.post("/upload", fileUploader.single("picture"), (req, res, next) => {
-//   // console.log("file is: ", req.file)
- 
-//   if (!req.file) {
-//     next(new Error("No file uploaded!"));
-//     return;
-//   }
-
-//   // Get the URL of the uploaded file and send it as a response.
-//   // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
-
-//   res.json({ fileUrl: req.file.path });
-// });
 
 module.exports = router;
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const isAuthenticated = require("../middleware/jwt.middleware");
 const User = require("../models/User.model");
 const List = require("../models/List.model")
+const fileUploader = require("../config/cloudinary.config")
 
 //http://localhost:5005/users/:username
 router.get("/:username", (req, res, next) => {
@@ -47,5 +48,37 @@ router.post("/:userId/edit", (req, res, next) => {
     })
     .catch((err) => next(err));
 });
+
+//http://localhost:5005/users/:userId/upload
+router.post("/:userId/upload", fileUploader.single("image"), (req, res, next) => {
+  let { userId } = req.params;
+  //if there was an image file uploaded in cloudinary, extracts the path so that it can be stored in the database
+  let picture = '';
+  if (req.file) {
+      picture = req.file.path;
+  }  
+  User.findByIdAndUpdate(userId, {picture: picture})
+  .then((resp) => {
+    res.json(resp);
+  })
+  .catch((err) => next(err));
+});
+
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("picture"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  
+  res.json({ fileUrl: req.file.path });
+});
+
 
 module.exports = router;
