0b85b033353c8605e07c83feb682b879bd63ca34
Raquel Barrio <r.barrio.f@gmail.com>
2023-06-08 16:54:59 +0200

    working on events detail

--- server/routes/events.routes.js
+++ server/routes/events.routes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
+const Event = require("../models/Event.model");
 // const { DateTime } = require("luxon");
 
 const isAuthenticated = require("../middleware/jwt.middleware");
 
-const Event = require("../models/Event.model");
 const { Router } = require("express");
 
 //CLOUDINARY
@@ -27,8 +27,14 @@ const { Router } = require("express");
 // });
 
 // GET EVENT
-router.get("/:eventId", (req, res, next) => {
-  
+router.get("/:eventId", isAuthenticated, (req, res, next) => {
+  let { eventId } = req.params;
+  Event.findById(eventId)
+  .populate("creator confirmedJoiners")
+  .then((resp) => {
+    res.json(resp);
+  })
+  .catch((err) => next(err));
 })
 
 // POST CREATE
